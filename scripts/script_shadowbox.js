2786d6c7e2a090c4125dd06bc1ee210a1a1b8956
[lesterbx] <[prz.lester@gmail.com]>
2017-05-26 19:23:07 +0200

    Shadowbox funcionando2

--- scripts/script_shadowbox.js
+++ scripts/script_shadowbox.js
@@ -1,21 +1,40 @@
 var currentImg;
+var images = [];
+
 function openBox(imgId, gallery){
+	currentImg = imgId-1;
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("GET", "../../galleries/"+gallery+".xml", true);
 	xhttp.send();
 	xhttp.onreadystatechange = function(){
-		alert(xhttp.responseText);
+		if (this.readyState == 4){
+			var parser = new DOMParser();
+			var xmlDoc = parser.parseFromString(xhttp.responseText, "text/xml");
+			var imagesXml = xmlDoc.getElementsByTagName('image');
+			for(var i = 0; i < imagesXml.length ; i++){
+				images.push('../'+imagesXml[i].getAttribute('file'))
+			}
+
+			document.getElementById('shadowbox_img').src = images[currentImg];
+			document.getElementById('shadowbox').style.display = "block";
+			
+		}
+		
 	}
 }
 
 function nextImg(){
-
+	currentImg++;
+	if(currentImg == images.length) currentImg = 0;
+	document.getElementById('shadowbox_img').src = images[currentImg];
 }
 
 function prevImg(){
-
+	currentImg--;
+	if(currentImg == -1) currentImg = images.length -1;
+	document.getElementById('shadowbox_img').src = images[currentImg];
 }
 
 function closeBox(){
-
+	document.getElementById('shadowbox').style.display = "none";
 }
